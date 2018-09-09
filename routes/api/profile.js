363ae739a6bba5340bd97206fe85c083de340f12
Unknown <nigelnaiguel.comp@gmail.com>
2018-09-09 02:59:37 -0300

    Populating current profile with reference user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -18,6 +18,7 @@ router.get('/test', (req, res) => res.json({msg: 'Profile works!'}));
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
     const errors  = {};
     Profile.findOne({user: req.user.id})
+        .populate('user', ['name', 'avatar'])
         .then(profile => {
             if(!profile){
                 errors.noprofile = 'There is no profile for this user'
